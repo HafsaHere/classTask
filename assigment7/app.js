097272c6b76d5cd49e0a6a5d9aa330376e1e830b
HafsaHere <143199963+HafsaHere@users.noreply.github.com>
2023-10-09 01:54:50 -0700

    Add files via upload

---
+++ assigment7/app.js
@@ -0,0 +1,15 @@
+// ---------------------------Chapter 46, 48 (Events)------------------------------------------
+
+// QUES NO:1
+
+function greet(){
+    alert ("welcome to assigment7");
+ }
+
+// QUES NO:7
+
+function msg(){
+    alert ("HELLO");
+ }
+
+//  QUES NO:
